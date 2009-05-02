7c3a6da49b6f24b0187413ec776e322f2e2b8d81
Jan Vesely <jano.vesely@gmail.com>
2009-05-02 08:49:53 +0000

    necessary changes for NetworkInfo
    git-svn-id: file:///var/svn/netsniffer/trunk@126 f984c181-819b-4b65-9e60-bf797f1e5738

--- headers/NetworkInfo.h
+++ headers/NetworkInfo.h
@@ -2,10 +2,43 @@
 
 #include "protocol.h"
 
-struct NetworkInfo{
+#define SAME_WAY(a,b) \
+	( a.protocol == b.protocol && \
+	a.sourceIP == b.sourceIP && \
+	a.destinationIP == b.destinationIP && \
+	a.sourcePort == b.sourcePort && \
+	a.destinationPort == b.destinationPort )
+
+#define BACK_WAY(a,b) \
+  ( a.protocol == b.protocol && \
+  a.sourceIP == b.destinationIP && \
+  a.destinationIP == b.sourceIP && \
+  a.sourcePort == b.destinationPort && \
+  a.destinationPort == b.sourcePort )
+
+
+struct NetworkInfo
+{
 	QHostAddress sourceIP;
   QHostAddress destinationIP;
   quint16 sourcePort;
   quint16 destinationPort;
   TrProtocol protocol;
 };
+/*----------------------------------------------------------------------------*/
+inline int qHash( const NetworkInfo& info )
+{
+	return qHash( info.sourceIP.toIPv4Address() ) ^
+		qHash( info.destinationIP.toIPv4Address() ) ^
+		qHash( info.sourcePort ) ^
+		qHash( info.destinationPort ) ^
+		qHash( (int)info.protocol );
+}
+/*----------------------------------------------------------------------------*/
+inline bool operator == ( const NetworkInfo& a, const NetworkInfo& b)
+{
+	return SAME_WAY(a,b) || BACK_WAY(a,b);
+}
+
+#undef SAME_WAY
+#undef BACK_WAY
