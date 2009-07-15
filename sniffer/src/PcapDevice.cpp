78a89a54dc38622e4d7e2511aa2d19fbdfb69cec
Jan Vesely <jano.vesely@gmail.com>
2009-07-15 11:37:59 +0200

    file doxygen header added

--- sniffer/src/PcapDevice.cpp
+++ sniffer/src/PcapDevice.cpp
@@ -1,3 +1,11 @@
+/*!
+ * @file
+ * @brief Short description.
+ *
+ * Long description. I would paste some Loren Ipsum rubbish here, but I'm afraid
+ * It would stay that way. Not that this comment is by any means ingenious but
+ * at least people can understand it.
+ */
 #include "PcapDevice.h"
 #include "Ethernet.h"
 
