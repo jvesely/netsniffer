fff5ca95d6ec8678f632bb311ef20d0d847362ba
Jan Vesely <jano.vesely@gmail.com>
2009-07-19 15:17:40 +0200

    IRecognizer documentation

--- headers/IRecognizer.h
+++ headers/IRecognizer.h
@@ -1,22 +1,61 @@
 /*!
  * @file
- * @brief Short description.
+ * @brief IRecognizer interface class.
  *
- * Long description. I would paste some Loren Ipsum rubbish here, but I'm afraid
- * It would stay that way. Not that this comment is by any means ingenious but
- * at least people can understand it.
+ * IRecognizer class shall provide interface for the manipulation with
+ * data recognizers. The class should identify itself by name and be able to
+ * analyze connections and present result sof this analysis.
  */
 #pragma once
 
 class IConnection;
 
+/*!
+ * @class IRecognizer IRecognizer.h "IRecognizer.h"
+ * @brief Recognizer interface class.
+ *
+ * Provides interface for Recognizer use and manipulation. Recognzier shall
+ * identify itself by name and be able to handle connection analysis.
+ */
 class IRecognizer: public QObject
 {
 public:
-	virtual ~IRecognizer(){};
+	/*!
+	 * @brief Retrieves recgonizer name.
+	 */
 	virtual const QString name() const = 0;
+
+	/*!
+	 * @brief Determines whether the recognizer would be able to analyze data.
+	 * @param connection Pointer to the connection to be analyzed.
+	 * @return True if the recognizer is recognized and will be able to 
+	 * analyze data.
+	 * @note The result of this function does not have to provide definite answer
+	 * to the question of beeing able to analyze the data.
+	 * @note This function does not change input connection in any way.
+	 */
 	virtual bool guess( const IConnection* connection ) = 0;
+
+	/*!
+	 * @brief Analyzes data of the connection.
+	 * @param connection Connection to be analyzed.
+	 * 
+	 * Retrives next packet from the connection and processes it in any way 
+	 * necessary.
+	 */
 	virtual bool parse( IConnection* connection ) = 0;
+
+	/*!
+	 * @brief Retrieves shorrt comment on the data in the connection.
+	 * @param connection Connection to comment on.
+	 * @return Comment produced by the recognizer.
+	 */
 	virtual QVariant comment( IConnection* connection ) = 0;
+
+	/*!
+	 * @brief Display user dialog containg detailed information on the connection.
+	 * @param connection Conneciton to view in detail.
+	 * @return True if datial dialog was successfully displayed, false otherwise.
+	 */
 	virtual bool showDetails( IConnection* connection ) = 0;
 };
